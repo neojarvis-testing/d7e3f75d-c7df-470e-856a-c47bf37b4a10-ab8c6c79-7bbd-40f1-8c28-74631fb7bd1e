36821a2a336351c32748068344a7fcc1fbcfbd11
samiksha1001 <samikshaganjane@gmail.com>
2025-04-12 12:22:25 +0000

    FeedBack Controller & Services

--- dotnetapp/Controllers/FeedbackController.cs
+++ dotnetapp/Controllers/FeedbackController.cs
@@ -1,8 +1,12 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using dotnetapp.Services;
+using dotnetapp.Models;
+using dotnetapp.Data;
 
 namespace dotnetapp.Controllers
 {
@@ -10,7 +14,71 @@ namespace dotnetapp.Controllers
     [Route("api/[controller]")]
     public class FeedbackController : ControllerBase
     {
-       
+        private readonly FeedbackService _feedbackService;
 
+        public FeedbackController(ApplicationDbContext context)
+        {
+            _feedbackService = new FeedbackService(context);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Feedback>>> GetAllFeedbacks()
+        {
+            try
+            {
+                var feedbacks = await _feedbackService.GetAllFeedbacks();
+                return Ok(feedbacks);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("user/{userId}")]
+        public async Task<ActionResult<IEnumerable<Feedback>>> GetFeedbacksByUserId(int userId)
+        {
+            try
+            {
+                var feedbacks = await _feedbackService.GetFeedbacksByUserId(userId);
+                return Ok(feedbacks);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddFeedback([FromBody] Feedback feedback)
+        {
+            try
+            {
+                await _feedbackService.AddFeedback(feedback);
+                return Ok("Feedback added successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{feedbackId}")]
+        public async Task<ActionResult> DeleteFeedback(int feedbackId)
+        {
+            try
+            {
+                var deleted = await _feedbackService.DeleteFeedback(feedbackId);
+                if (!deleted)
+                {
+                    return NotFound("Feedback not found");
+                }
+                return Ok("Feedback deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
