f0bad5a1b79ed0397eefd33839760cce39a7b190
jindalankit2466 <jindalankit136@gmail.com>
2025-04-12 05:26:15 +0000

    model commit

--- dotnetapp/Models/User.cs
+++ dotnetapp/Models/User.cs
@@ -7,9 +7,13 @@ namespace dotnetapp.Models
 {
     public class User
     {
-        public string Username { get; set;}
+        public int UserId { get; set;}
         public string Email { get; set;}
         public string Password { get; set; }
-        public string UserRole { get; set; }
+        public string Username { get; set; }
+        public string MobileNumber {get;set;}
+        public string UserRole {get;set;}
     }
 }
+
+
