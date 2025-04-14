6111d85f1c26c7047d35a03039607f4dfbd21f93
samiksha1001 <samikshaganjane@gmail.com>
2025-04-14 04:13:25 +0000

    er

--- dotnetapp/Models/Room.cs
+++ dotnetapp/Models/Room.cs
@@ -18,7 +18,7 @@ namespace dotnetapp.Models
     [Required(ErrorMessage = "Room Type is required.")]
     public string RoomType { get; set; }
 
-    [Range(1, 1000, ErrorMessage = "Number of Rooms must be between 1 and 1000.")]
+    [Range(1, 10, ErrorMessage = "Number of Rooms must be between 1 and 10.")]
     public int NoOfRooms { get; set; }
 
     [Range(0.1, double.MaxValue, ErrorMessage = "Price Per Night must be greater than zero.")]
