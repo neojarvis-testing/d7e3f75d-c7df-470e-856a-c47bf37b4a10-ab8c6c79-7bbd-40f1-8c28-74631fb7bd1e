480e53714f26444f9a99c550fab9c202616ca894
jindalankit2466 <jindalankit136@gmail.com>
2025-04-15 11:02:14 +0000

    admin add room and admin edit room

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -3,16 +3,42 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { ErrorComponent } from './components/error/error.component';
+import { AuthGuard } from './components/authguard/auth.guard';
+import { AdminaddroomComponent } from './components/adminaddroom/adminaddroom.component';
+import { AdminviewroomComponent } from './components/adminviewroom/adminviewroom.component';
+import { AdmineditroomComponent } from './components/admineditroom/admineditroom.component';
+import { AdminviewrequestedbookingComponent } from './components/adminviewrequestedbooking/adminviewrequestedbooking.component';
+import { AdminviewfeedbackComponent } from './components/adminviewfeedback/adminviewfeedback.component';
+import { UseraddbookingComponent } from './components/useraddbooking/useraddbooking.component';
+import { UserviewroomComponent } from './components/userviewroom/userviewroom.component';
+import { UserviewmybookingComponent } from './components/userviewmybooking/userviewmybooking.component';
+import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfeedback.component';
+import { UserviewfeedbackComponent } from './components/userviewfeedback/userviewfeedback.component';
+ 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegistrationComponent},
-  {path: 'home'  , component: HomeComponent},
-  {path: '', redirectTo: '/home',  pathMatch: 'full'}, 
-];
-
+  {path: 'error', component: ErrorComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'addAdminRoom', component: AdminaddroomComponent, canActivate: [AuthGuard]},
+  {path: 'adminviewroom', component: AdminviewroomComponent, canActivate: [AuthGuard]},
+  {path: 'admineditRoom/:roomId', component: AdmineditroomComponent, canActivate: [AuthGuard]},
+  {path: 'adminviewrequestedbooking', component: AdminviewrequestedbookingComponent, canActivate: [AuthGuard]},
+  {path: 'adminviewfeedback', component: AdminviewfeedbackComponent, canActivate: [AuthGuard]},
+  {path: 'useraddbooking/:id', component: UseraddbookingComponent, canActivate: [AuthGuard]},
+  {path: 'userviewroom', component: UserviewroomComponent, canActivate: [AuthGuard]},
+  {path: 'userviewmybooking', component: UserviewmybookingComponent, canActivate: [AuthGuard]},
+  {path: 'useraddfeedback', component: UseraddfeedbackComponent, canActivate: [AuthGuard]},
+  {path: 'userviewfeedback', component: UserviewfeedbackComponent, canActivate: [AuthGuard]},
+ 
+]
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
+ 
--- angularapp/src/app/services/room.service.ts
+++ angularapp/src/app/services/room.service.ts
@@ -8,7 +8,7 @@ import { Booking } from '../models/booking.model';
   providedIn: 'root',
 })
 export class RoomService {
-  public apiUrl = 'http://8080-your-backend-url.premiumproject.examly.io/api'; 
+  public apiUrl = 'https://8080-adaaebcbceaecbccefdafbeedadabccbbdfcfbbde.premiumproject.examly.io/api'; 
 
   constructor(private http: HttpClient) {}
 
@@ -28,7 +28,7 @@ export class RoomService {
   }
 
   
-  getRoomById(roomId: string): Observable<Room> {
+  getRoomById(roomId: number): Observable<Room> {
     return this.http.get<Room>(`${this.apiUrl}/Room/${roomId}`, {
       headers: this.getAuthHeaders(),
     });
@@ -42,14 +42,14 @@ export class RoomService {
   }
 
 
-  updateRoom(roomId: string, room: Room): Observable<Room> {
+  updateRoom(roomId: number, room: Room): Observable<Room> {
     return this.http.put<Room>(`${this.apiUrl}/Room/${roomId}`, room, {
       headers: this.getAuthHeaders(),
     });
   }
 
 
-  deleteRoom(roomId: string): Observable<void> {
+  deleteRoom(roomId: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/Room/${roomId}`, {
       headers: this.getAuthHeaders(),
     });
@@ -62,7 +62,7 @@ export class RoomService {
     });
   }
 
-  getBookingsByUserId(userId: string): Observable<Booking[]> {
+  getBookingsByUserId(userId: number): Observable<Booking[]> {
     return this.http.get<Booking[]>(`${this.apiUrl}/Booking/user/${userId}`, {
       headers: this.getAuthHeaders(),
     });
@@ -75,13 +75,13 @@ export class RoomService {
   }
 
 
-  updateBooking(bookingId: string, booking: Booking): Observable<Booking> {
+  updateBooking(bookingId: number, booking: Booking): Observable<Booking> {
     return this.http.put<Booking>(`${this.apiUrl}/Booking/${bookingId}`, booking, {
       headers: this.getAuthHeaders(),
     });
   }
 
-  deleteBooking(bookingId: string): Observable<void> {
+  deleteBooking(bookingId: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/Booking/${bookingId}`, {
       headers: this.getAuthHeaders(),
     });
