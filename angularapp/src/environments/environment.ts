3a062398b0430b87d3bf1f667fdfc67052662fa2
onedeadgo <saurabhpatel5645@gmail.com>
2025-04-18 10:12:14 +0000

    environment committing

--- angularapp/src/environments/environment.ts
+++ angularapp/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: "https://8080-bdcbecadabbaecbccefdafbeedadabccbbdfcfbbde.premiumproject.examly.io"
 };
 
 /*
