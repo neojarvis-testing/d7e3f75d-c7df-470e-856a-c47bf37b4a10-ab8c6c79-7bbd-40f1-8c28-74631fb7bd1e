6e84072713c4498937533ea06d7aa64b110d1986
devankawasthy <adevank@gmail.com>
2025-04-18 10:02:16 +0000

    base url done

--- angularapp/src/app/services/feedback.service.ts
+++ angularapp/src/app/services/feedback.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Feedback } from '../models/feedback.model'; // Make sure this model is defined
+import { environment } from 'src/environments/environment';
 
 @Injectable({
 providedIn: 'root'
 })
 export class FeedbackService {
   
+    public apiUrl = environment.baseUrl;
 
 constructor(private http: HttpClient) {}
 
@@ -21,25 +23,25 @@ Authorization: `Bearer ${token}`,
 }
 // Send feedback to the server
 sendFeedback(feedback: Feedback): Observable<Feedback> {
-return this.http.post<Feedback>(`${this.apiUrl}/Feedback`, feedback, {
+return this.http.post<Feedback>(`${this.apiUrl}/api/Feedback`, feedback, {
 headers: this.getAuthHeaders()
 });
 }
 // Get all feedbacks submitted by a specific user
 getAllFeedbacksByUserId(userId: number): Observable<Feedback[]> {
-return this.http.get<Feedback[]>(`${this.apiUrl}/Feedback/user/${userId}`, {
+return this.http.get<Feedback[]>(`${this.apiUrl}/api/Feedback/user/${userId}`, {
 headers: this.getAuthHeaders()
 });
 }
 // Delete feedback by ID
 deleteFeedback(feedbackId: number): Observable<void> {
-return this.http.delete<void>(`${this.apiUrl}/Feedback/${feedbackId}`, {
+return this.http.delete<void>(`${this.apiUrl}/api/Feedback/${feedbackId}`, {
 headers: this.getAuthHeaders()
 });
 }
 // Get all feedbacks from the server
 getFeedbacks(): Observable<Feedback[]> {
-return this.http.get<Feedback[]>(`${this.apiUrl}/Feedback`, {
+return this.http.get<Feedback[]>(`${this.apiUrl}/api/Feedback`, {
 headers: this.getAuthHeaders()
 });
 }
