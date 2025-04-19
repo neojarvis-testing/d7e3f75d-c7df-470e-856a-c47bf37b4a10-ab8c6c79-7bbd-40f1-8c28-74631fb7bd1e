7a67e58f393f9133e2192b5751562c58c1869973
devankawasthy <adevank@gmail.com>
2025-04-19 13:38:34 +0000

    last committ

--- angularapp/src/environments/environment.ts
+++ angularapp/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   
-  baseUrl: "https://8080-cbdfbddecddaecbccefdafbeedadabccbbdfcfbbde.premiumproject.examly.io"
-  // baseUrl: "https://8080-deafabfcaecbccefdafbeedadabccbbdfcfbbde.premiumproject.examly.io"
+  // baseUrl: "https://8080-cbdfbddecddaecbccefdafbeedadabccbbdfcfbbde.premiumproject.examly.io"
+  baseUrl: "https://8080-deafabfcaecbccefdafbeedadabccbbdfcfbbde.premiumproject.examly.io"
 
 };
 
